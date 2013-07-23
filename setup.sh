1a56c5bfcf2dd6ead3b711751ee319ad0ad8c555
Ubuntu <ubuntu@ip-172-31-6-70.us-west-1.compute.internal>
2013-07-23 23:41:57 +0000

    Node shit

--- setup.sh
+++ setup.sh
@@ -8,10 +8,12 @@ sudo apt-get install -y git
 sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+# Install Node...
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
